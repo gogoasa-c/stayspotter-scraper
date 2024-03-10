7fb4e772769ab801f661bed57eb115308b4c98c3
Cristian Gogoasa <gogoasac16@gmail.com>
2024-03-10 14:37:30 +0200

    feat: register flask app with eureka service discovery

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import logging.config
+import py_eureka_client.eureka_client as eureka_client
 
 from flask import Flask
 from pyms.flask.app import Microservice
@@ -11,6 +12,11 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+eureka_client.init(eureka_server="http://localhost:8761",
+                   app_name="ss-scraper",
+                   instance_port=8090,
+                   instance_host="127.0.0.1")
+
 
 class SSScraperMicroservice(Microservice):
     def init_logger(self):
@@ -20,7 +26,6 @@ class SSScraperMicroservice(Microservice):
             with open('./config/logging_config.json', 'r') as f:
                 LOGGING = json.load(f)
                 logging.config.dictConfig(LOGGING)
-
 
     def create_app(self) -> Flask:
         app = super().create_app()
